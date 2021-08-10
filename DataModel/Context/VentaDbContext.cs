256f67365ec3a8a41750428152ce11c0fe44ad8e
Jeremy Mezon de la Cruz <51011294+JeremyMezon@users.noreply.github.com>
2021-08-09 21:30:41 -0400

    Configuracion del DBContext

---
+++ DataModel/Context/VentaDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace VentaVehiculos.DataModel.Context
+{
+    class VentaDbContext:DbContext
+    {
+        public VentaDbContext()
+            :base("name=conexionBD")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { set; get; }
+        public DbSet<Vehiculo> Vehiculos { set; get; }
+        public DbSet<Modelo> Modelos { set; get; }
+        public DbSet<Marca> Marcas { set; get; }
+        public DbSet<Venta> Ventas { set; get; }
+        public DbSet<TipoTransimision> TipoTransimisions { set; get; }
+        public DbSet<TipoCombustible> TipoCombustibles { set; get; }
+    }
+}
