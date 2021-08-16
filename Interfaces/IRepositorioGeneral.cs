047288a9e06512ac06e42111438b8523b60e9d55
Jeremy Mezon de la Cruz <51011294+JeremyMezon@users.noreply.github.com>
2021-08-16 12:50:37 -0400

    Interfaz y Repositorio General

---
+++ Interfaces/IRepositorioGeneral.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaVehiculos.Interfaces
+{
+    interface IRepositorioGeneral<T>
+    {
+        T Crear(T cliente);
+        List<T> GetAll();
+        T FIndById(int ID);
+        ResultadoOperacion Modificar(T cliente);
+        ResultadoOperacion Eliminar(T cliente);
+    }
+}
---
+++ ResultadoOperacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaVehiculos
+{
+    class ResultadoOperacion
+    {
+        public static bool Success { get; set; }
+        public static string Message { get; set; }
+        public static object Data { get; set; }
+    }
+}
