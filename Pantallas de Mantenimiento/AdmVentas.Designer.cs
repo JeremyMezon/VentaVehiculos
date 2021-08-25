1b33666becb592ea8d027f4c237cd7b07a1981aa
Freddy Soto <45477077+freddysotof@users.noreply.github.com>
2021-08-25 09:51:12 -0400

    Pantalla de venta de vehiculo

--- Pantallas de Mantenimiento/AdmVentas.Designer.cs
+++ Pantallas de Mantenimiento/AdmVentas.Designer.cs
@@ -28,12 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bunifuSeparator1 = new Bunifu.Framework.UI.BunifuSeparator();
+            this.LbTituloVenta = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboClientes = new System.Windows.Forms.ComboBox();
+            this.comboVehiculo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboMetodoPago = new System.Windows.Forms.ComboBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGuardarVenta = new System.Windows.Forms.Button();
+            this.comboEstado = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bunifuSeparator1
+            // 
+            this.bunifuSeparator1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuSeparator1.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(211)))), ((int)(((byte)(211)))));
+            this.bunifuSeparator1.LineThickness = 1;
+            this.bunifuSeparator1.Location = new System.Drawing.Point(19, 91);
+            this.bunifuSeparator1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.bunifuSeparator1.Name = "bunifuSeparator1";
+            this.bunifuSeparator1.Size = new System.Drawing.Size(550, 27);
+            this.bunifuSeparator1.TabIndex = 35;
+            this.bunifuSeparator1.Transparency = 255;
+            this.bunifuSeparator1.Vertical = false;
+            // 
+            // LbTituloVenta
+            // 
+            this.LbTituloVenta.AutoSize = true;
+            this.LbTituloVenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbTituloVenta.Location = new System.Drawing.Point(13, 34);
+            this.LbTituloVenta.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LbTituloVenta.Name = "LbTituloVenta";
+            this.LbTituloVenta.Size = new System.Drawing.Size(271, 48);
+            this.LbTituloVenta.TabIndex = 34;
+            this.LbTituloVenta.Text = "Nueva Venta";
+            this.LbTituloVenta.Click += new System.EventHandler(this.LbTituloMarca_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(21, 127);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 29);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Cliente:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // comboClientes
+            // 
+            this.comboClientes.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.comboClientes.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboClientes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboClientes.FormattingEnabled = true;
+            this.comboClientes.Location = new System.Drawing.Point(247, 131);
+            this.comboClientes.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboClientes.Name = "comboClientes";
+            this.comboClientes.Size = new System.Drawing.Size(302, 28);
+            this.comboClientes.TabIndex = 44;
+            this.comboClientes.SelectedIndexChanged += new System.EventHandler(this.comboClientes_SelectedIndexChanged);
+            // 
+            // comboVehiculo
+            // 
+            this.comboVehiculo.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.comboVehiculo.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboVehiculo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboVehiculo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboVehiculo.FormattingEnabled = true;
+            this.comboVehiculo.Location = new System.Drawing.Point(247, 182);
+            this.comboVehiculo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboVehiculo.Name = "comboVehiculo";
+            this.comboVehiculo.Size = new System.Drawing.Size(302, 28);
+            this.comboVehiculo.TabIndex = 46;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 178);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 29);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "Vehiculo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 239);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 29);
+            this.label2.TabIndex = 47;
+            this.label2.Text = "Metodo de Pago:";
+            // 
+            // comboMetodoPago
+            // 
+            this.comboMetodoPago.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.comboMetodoPago.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboMetodoPago.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboMetodoPago.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboMetodoPago.FormattingEnabled = true;
+            this.comboMetodoPago.Items.AddRange(new object[] {
+            "Efectivo",
+            "Tarjeta",
+            "Cheque"});
+            this.comboMetodoPago.Location = new System.Drawing.Point(247, 239);
+            this.comboMetodoPago.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboMetodoPago.Name = "comboMetodoPago";
+            this.comboMetodoPago.Size = new System.Drawing.Size(302, 28);
+            this.comboMetodoPago.TabIndex = 49;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(247, 298);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPrecio.MaxLength = 100;
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(302, 26);
+            this.txtPrecio.TabIndex = 50;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 298);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 29);
+            this.label3.TabIndex = 51;
+            this.label3.Text = "Precio RD$:";
+            // 
+            // btnGuardarVenta
+            // 
+            this.btnGuardarVenta.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnGuardarVenta.FlatAppearance.BorderSize = 0;
+            this.btnGuardarVenta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardarVenta.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarVenta.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGuardarVenta.Location = new System.Drawing.Point(339, 429);
+            this.btnGuardarVenta.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnGuardarVenta.Name = "btnGuardarVenta";
+            this.btnGuardarVenta.Size = new System.Drawing.Size(210, 54);
+            this.btnGuardarVenta.TabIndex = 52;
+            this.btnGuardarVenta.Text = "Guardar";
+            this.btnGuardarVenta.UseVisualStyleBackColor = false;
+            this.btnGuardarVenta.Click += new System.EventHandler(this.btnGuardarVenta_Click);
+            // 
+            // comboEstado
+            // 
+            this.comboEstado.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.comboEstado.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboEstado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboEstado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboEstado.FormattingEnabled = true;
+            this.comboEstado.Items.AddRange(new object[] {
+            "Activo",
+            "Inactivo"});
+            this.comboEstado.Location = new System.Drawing.Point(247, 344);
+            this.comboEstado.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboEstado.Name = "comboEstado";
+            this.comboEstado.Size = new System.Drawing.Size(302, 28);
+            this.comboEstado.TabIndex = 54;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(21, 344);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 29);
+            this.label7.TabIndex = 53;
+            this.label7.Text = "Estado";
+            // 
+            // AdmVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(584, 497);
+            this.Controls.Add(this.comboEstado);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnGuardarVenta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.comboMetodoPago);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboVehiculo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboClientes);
+            this.Controls.Add(this.bunifuSeparator1);
+            this.Controls.Add(this.LbTituloVenta);
+            this.Controls.Add(this.label5);
+            this.Name = "AdmVentas";
             this.Text = "AdmVentas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Bunifu.Framework.UI.BunifuSeparator bunifuSeparator1;
+        public System.Windows.Forms.Label LbTituloVenta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboClientes;
+        private System.Windows.Forms.ComboBox comboVehiculo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboMetodoPago;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGuardarVenta;
+        private System.Windows.Forms.ComboBox comboEstado;
+        private System.Windows.Forms.Label label7;
     }
 }
--- Pantallas de Mantenimiento/AdmVentas.cs
+++ Pantallas de Mantenimiento/AdmVentas.cs
@@ -7,14 +7,151 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VentaVehiculos.DataModel.Entities;
+using VentaVehiculos.Repositorios;
 
 namespace VentaVehiculos.Pantallas_de_Mantenimiento
 {
     public partial class AdmVentas : Form
     {
+        private Venta venta { get; set; }
+        private bool esModificacion = false;
+
+        private void AdmVentas_Load(object sender, EventArgs e)
+        {
+
+            ClienteRepositorio clienteRepositorio = new ClienteRepositorio();
+            comboClientes.DisplayMember = "Nombre";
+            comboClientes.ValueMember = "Id";
+            comboClientes.DataSource = clienteRepositorio.GetAll();
+
+
+            VehiculoRepositorio vehiculoRepositorio = new VehiculoRepositorio();
+            comboVehiculo.DisplayMember = "Nombre";
+            comboVehiculo.ValueMember = "Id";
+            comboVehiculo.DataSource = vehiculoRepositorio.GetAll();
+
+
+        }
+
         public AdmVentas()
         {
             InitializeComponent();
+            comboEstado.SelectedIndex = 0;
+        }
+        public AdmVentas(Venta _venta)
+        {
+            InitializeComponent();
+            LbTituloVenta.Text = "Modificar Venta";
+            this.venta = _venta;
+            comboClientes.SelectedIndex = int.Parse(comboClientes.SelectedItem.ToString());
+            comboVehiculo.SelectedIndex = int.Parse(comboVehiculo.SelectedItem.ToString());
+            comboMetodoPago.SelectedIndex = int.Parse(comboMetodoPago.SelectedItem.ToString());
+            txtPrecio.Text = _venta.Precio.ToString();
+            if (_venta.Estatus == "AC")
+            {
+                comboEstado.SelectedItem = "Activo";
+            }
+            else
+            {
+                comboEstado.SelectedItem = "Inactivo";
+            }
+            esModificacion = true;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LbTituloMarca_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardarVenta_Click(object sender, EventArgs e)
+        {
+            if (validacionVentas())
+            {
+                VentaRepositorio ventaRepositorio = new VentaRepositorio();
+                Venta venta = this.capturarVenta();
+
+                if (this.esModificacion)
+                {
+                    ventaRepositorio.Modificar(venta);
+                    MessageBox.Show("Datos actualizados correctamente", "Datos Actualizados", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    var feedback = ventaRepositorio.Crear(venta);
+
+                    if (feedback.Success)
+                    {
+                        MessageBox.Show(feedback.Message, "Datos Guardados", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show(feedback.Message, "No se pudo guardar los datos", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    }
+                }
+            }
+        }
+
+        Venta capturarVenta()
+        {
+            Venta venta = new Venta();
+
+            string estado = comboEstado.SelectedItem.ToString().Substring(0, 2).ToUpper();
+
+            if (esModificacion)
+            {
+                venta.Id = this.venta.Id;
+            }
+
+            venta.ClienteId = int.Parse(comboClientes.SelectedItem.ToString());
+            venta.VehiculoId = int.Parse(comboVehiculo.SelectedItem.ToString());
+            venta.MetodoPago = comboMetodoPago.SelectedItem.ToString();
+            venta.Precio = int.Parse(txtPrecio.Text);
+            venta.Estatus = estado;
+            venta.Borrado = false;
+            venta.FechaRegistro = esModificacion ? this.venta.FechaRegistro : DateTime.Now;
+            venta.FechaActualizacion = DateTime.Now;
+
+
+
+            return venta;
+        }
+
+        bool validacionVentas()
+        {
+            if (comboClientes.SelectedIndex <0)
+            {
+                MessageBox.Show("Debe seleccionar un cliente", "Campos vacios", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            else if (comboVehiculo.SelectedIndex < 0)
+            {
+                MessageBox.Show("Debe seleccionar un cliente", "Campos vacios", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            else if (comboMetodoPago.SelectedIndex < 0)
+            {
+                MessageBox.Show("Debe seleccionar el metodo de pago", "Campos vacios", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            else if (string.IsNullOrWhiteSpace(txtPrecio.Text))
+            {
+                MessageBox.Show("Debe digitar el precio del vehiculo a vender", "Campos vacios", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            
+            return true;
+        }
+
+        private void comboClientes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
